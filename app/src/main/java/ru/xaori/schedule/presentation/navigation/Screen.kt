db877c1d13f825e5ce647da1ea114ab2090a06df
xaoriGit <xaorinit@gmail.com>
2025-11-02 20:04:32 +0600

    feat: bottom sheet выбора расписания

--- app/src/main/java/ru/xaori/schedule/presentation/navigation/Screen.kt
+++ app/src/main/java/ru/xaori/schedule/presentation/navigation/Screen.kt
@@ -6,6 +6,4 @@ import kotlinx.serialization.Serializable
 sealed class Screen(val route: String) {
     object Start : Screen("start")
     object Schedule : Screen("schedule")
-    @Serializable
-    data class ClientChoice(val showCancelButton: Boolean = true) : Screen("client_choice")
 }
